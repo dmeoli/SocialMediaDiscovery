b344167e795da566e55362a3ce5633ea56f13f72
Donato Meoli <donato.meoli.95@gmail.com>
2018-10-22 07:23:21 +0200

    fixed IllegalArgsExc in Emotion

--- src/main/java/edu/uniba/di/lacam/kdde/donato/meoli/preprocessing/nlp/feature/discrete/Emotion.java
+++ src/main/java/edu/uniba/di/lacam/kdde/donato/meoli/preprocessing/nlp/feature/discrete/Emotion.java
@@ -1,14 +1,15 @@
 package edu.uniba.di.lacam.kdde.donato.meoli.preprocessing.nlp.feature.discrete;
 
+import edu.stanford.nlp.ling.WordLemmaTag;
 import edu.uniba.di.lacam.kdde.WNAffect;
 import edu.uniba.di.lacam.kdde.donato.meoli.preprocessing.database.mongodb.domain.Post;
 import edu.uniba.di.lacam.kdde.donato.meoli.preprocessing.nlp.feature.POSTag;
 import edu.uniba.di.lacam.kdde.lexical_db.MITWordNet;
 import edu.uniba.di.lacam.kdde.util.WNAffectConfiguration;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Emotion extends DiscreteContentBasedFeatureExtraction {
 
@@ -24,14 +25,28 @@ public class Emotion extends DiscreteContentBasedFeatureExtraction {
 
     @Override
     public Set<String> extractFeature(Post postX, Post postY) {
-        Set<String> emotionsPostX = postX.getBodyPOSTags().parallelStream().map(wordLemmaTagX ->
-                wnAffect.getParent(wnAffect.getEmotion(wordLemmaTagX.lemma(), Objects.requireNonNull(
-                        POSTag.getPOS(wordLemmaTagX.tag()))), EMOTION_PARENT_LEVEL)).filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-        Set<String> emotionsPostY = postY.getBodyPOSTags().parallelStream().map(wordLemmaTagY ->
-                wnAffect.getParent(wnAffect.getEmotion(wordLemmaTagY.lemma(), Objects.requireNonNull(
-                        POSTag.getPOS(wordLemmaTagY.tag()))), EMOTION_PARENT_LEVEL)).filter(Objects::nonNull)
-                .collect(Collectors.toSet());
+        Set<String> emotionsPostX = new HashSet<>();
+        for (WordLemmaTag wordLemmaTagX : postX.getBodyPOSTags()) {
+            String parent;
+            try {
+                parent = wnAffect.getParent(wnAffect.getEmotion(wordLemmaTagX.lemma(), Objects.requireNonNull(
+                        POSTag.getPOS(wordLemmaTagX.tag()))), EMOTION_PARENT_LEVEL);
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            if (Objects.nonNull(parent)) emotionsPostX.add(parent);
+        }
+        Set<String> emotionsPostY = new HashSet<>();
+        for (WordLemmaTag wordLemmaTagY : postY.getBodyPOSTags()) {
+            String parent;
+            try {
+                parent = wnAffect.getParent(wnAffect.getEmotion(wordLemmaTagY.lemma(), Objects.requireNonNull(
+                        POSTag.getPOS(wordLemmaTagY.tag()))), EMOTION_PARENT_LEVEL);
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            if (Objects.nonNull(parent)) emotionsPostY.add(parent);
+        }
         emotionsPostX.retainAll(emotionsPostY);
         return emotionsPostX;
     }
