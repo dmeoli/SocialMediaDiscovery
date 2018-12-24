1daff22ad1fd91dadb219eb8f81c4b3eb1f41146
Donato Meoli <donato.meoli.95@gmail.com>
2018-12-24 10:49:23 +0100

    updated collection name results

--- src/main/java/edu/uniba/di/lacam/kdde/donato/meoli/discovery/database/mongo/SocialMediaDiscoveryResult.java
+++ src/main/java/edu/uniba/di/lacam/kdde/donato/meoli/discovery/database/mongo/SocialMediaDiscoveryResult.java
@@ -3,6 +3,7 @@ package edu.uniba.di.lacam.kdde.donato.meoli.discovery.database.mongo;
 import com.google.gson.GsonBuilder;
 import edu.uniba.di.lacam.kdde.donato.meoli.database.mongo.MongoDAO;
 import edu.uniba.di.lacam.kdde.donato.meoli.discovery.database.mongo.domain.Result;
+import edu.uniba.di.lacam.kdde.donato.meoli.util.SocialMediaDiscoveryConfiguration;
 import org.bson.Document;
 
 public class SocialMediaDiscoveryResult extends MongoDAO {
@@ -10,8 +11,18 @@ public class SocialMediaDiscoveryResult extends MongoDAO {
     private static final String RESULTS_COLLECTION = "results";
 
     public void insertResult(Result result, int cumulativeTemporalGraphMinutes, int temporalSubGraphsMinutes, float minSupport) {
-        getCollection(RESULTS_COLLECTION + SEPARATOR + cumulativeTemporalGraphMinutes + SEPARATOR +
-                temporalSubGraphsMinutes + SEPARATOR + minSupport).insertOne(Document.parse(
+        StringBuilder sb = new StringBuilder();
+        sb.append(RESULTS_COLLECTION).append(SEPARATOR);
+        if (SocialMediaDiscoveryConfiguration.getInstance().useLexicalSimilarityLinks())
+            sb.append("l").append(SEPARATOR);
+        if (SocialMediaDiscoveryConfiguration.getInstance().useSemanticSimilarityLinks())
+            sb.append("s").append(SEPARATOR);
+        if (SocialMediaDiscoveryConfiguration.getInstance().useEmotionalLinks())
+            sb.append("e").append(SEPARATOR);
+        sb.append(cumulativeTemporalGraphMinutes).append(SEPARATOR)
+                .append(temporalSubGraphsMinutes).append(SEPARATOR)
+                .append(minSupport);
+        getCollection(sb.toString()).insertOne(Document.parse(
                 new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().toJson(result)));
     }
 }
